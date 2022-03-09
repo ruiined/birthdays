4355c2acc90de59488a9c7d2d27dac343b63cfc3
ruiined <ruiined@gmail.com>
2022-03-09 12:08:02 +0000

    started out birthday list challenge

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,20 @@
+require 'Date'
 
+class BirthdayList
+  def initialize
+    @birthdays = [
+      { name: "Queen Fly", birthday: Date.new(1979, 3, 12) },
+      { name: "King Bread", birthday: Date.new(1999, 2, 21) },
+      { name: "Prince Cheese", birthday: Date.new(1897, 12,1) },
+      { name: "Princess Moss", birthday: Date.new(1345, 2, 15) }
+    ]
+  end
+
+  def list
+    @birthdays.each { |person| puts "#{person[:name]}'s birthday: #{person[:birthday]}" }
+  end
+
+  def add(name, date)
+    @birthdays << { name: name, birthday: Date.new(date) }
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,22 @@
+require 'birthday_list'
 
+describe BirthdayList do
+  #let(:list) { described_class.new }
+
+  # subject { described_class.new }
+
+  # it { is_expected.to_not raise_error }
+  it "stores birthdays" do
+    expect { subject }.to_not raise_error
+  end
+
+  it "lists birthdays in a tidy format" do
+    expect { subject.list }.to output(String).to_stdout
+  end
+
+  it "adds a birthday to a list" do
+    x = subject.add("Me", (202058))
+    expect(subject.list).to include x
+  end
+
+end
